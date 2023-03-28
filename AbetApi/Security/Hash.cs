56213ac72312a003d101900b8c2db75968620f36
Laymon, Michael <michaellaymon@my.unt.edu>
2023-03-27 20:47:38 -0500

    Bug fix: Convert to static members - Was not able to call function without an object - Calling function directly is simpler

--- AbetApi/Security/Hash.cs
+++ AbetApi/Security/Hash.cs
@@ -6,20 +6,15 @@ namespace AbetApi.Security {
 
     public class Hash {
 
-        private readonly string _salt;
-        private readonly int _iterations;
+        public static string Encrypt(string text) {
 
-        public Hash() {
-            _salt = Environment.GetEnvironmentVariable("HASH_SALT"); // the key 
-            _iterations = Convert.ToInt32(Environment.GetEnvironmentVariable("HASH_ITERATIONS")); // the number of times the hash will be performed
-        }
-
-        public string Encrypt(string text) {
+            string _SALT = Environment.GetEnvironmentVariable("HASH_SALT"); // the key 
+            int _ITERATIONS = Convert.ToInt32(Environment.GetEnvironmentVariable("HASH_ITERATIONS")); // the number of times the hash will be performed
 
             using (var sha256 = SHA256.Create()) { // using instance of SHA256 algorithm as sha256
-                byte[] hash = Encoding.UTF8.GetBytes(text + _salt); // append the salt and convert to bytearray
+                byte[] hash = Encoding.UTF8.GetBytes(text + _SALT); // append the salt and convert to bytearray
 
-                for (int i = 0; i < _iterations; i++) // for n iterations...
+                for (int i = 0; i < _ITERATIONS; i++) // for n iterations...
                     hash = sha256.ComputeHash(hash); // hash once using sha256
 
                 return Convert.ToBase64String(hash); // return hashed result as (base64) string
@@ -28,13 +23,16 @@ namespace AbetApi.Security {
         
         }
 
-        public string Decrypt(string hash) {
+        public static string Decrypt(string hash) {
+
+            string _SALT = Environment.GetEnvironmentVariable("HASH_SALT"); // the key 
+            int _ITERATIONS  = Convert.ToInt32(Environment.GetEnvironmentVariable("HASH_ITERATIONS")); // the number of times the hash will be performed
 
             using (var sha256 = SHA256.Create()) { // using instance of SHA256 algorithm as sha256
                 byte[] decodedHash = Convert.FromBase64String(hash); // convert from (base64) string to bytearray
-                byte[] originalHash = Encoding.UTF8.GetBytes(Convert.ToBase64String(decodedHash) + _salt); // append the salt and convert to bytearray
+                byte[] originalHash = Encoding.UTF8.GetBytes(Convert.ToBase64String(decodedHash) + _SALT); // append the salt and convert to bytearray
 
-                for (int i = 0; i < _iterations; i++) // for n iterations...
+                for (int i = 0; i < _ITERATIONS; i++) // for n iterations...
                     originalHash = sha256.ComputeHash(originalHash); // unhash once using sha256
 
                 return Encoding.UTF8.GetString(originalHash); // return hashed result as (UTF-8) string
