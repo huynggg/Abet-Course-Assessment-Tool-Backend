03bef4397ae4c78d172d078e0ab5b08840fde6ce
Laymon, Michael <michaellaymon@my.unt.edu>
2023-03-27 19:59:55 -0500

    Create new hash encryption algorithm - Simpler algorithm uses sha256 - Data is salted and the hash iterates multiple times

---
+++ AbetApi/Security/Hash.cs
@@ -0,0 +1,34 @@
+﻿using System.Security.Cryptography; // AES, CryptoStream, Rfc2898DeriveBytes
+using System.Text; // UTF8 Encoding
+using System; // Convert
+
+namespace AbetApi.Security {
+
+    public class Hash {
+
+        private readonly string _salt;
+        private readonly int _iterations;
+
+        public Hash() {
+            // Retrieve the key from a local environment variable
+            _salt = Environment.GetEnvironmentVariable("HASH_SALT"); // the key 
+            _iterations = Convert.ToInt32(Environment.GetEnvironmentVariable("HASH_ITERATIONS")); // the number of times the hash will be performed
+        }
+
+        public string Encrypt(string text) {
+
+            using (var sha256 = SHA256.Create()) { // using instance of SHA256 algorithm as sha256
+                byte[] hash = Encoding.UTF8.GetBytes(text + _salt); // append the salt and convert to bytearray
+
+                for (int i = 0; i < _iterations; i++) // for n iterations...
+                    hash = sha256.ComputeHash(hash); // hash once using sha256
+
+                return Convert.ToBase64String(hash); // return hashed result as (BASE64) string
+
+            }
+        
+        }
+
+    }
+
+}
